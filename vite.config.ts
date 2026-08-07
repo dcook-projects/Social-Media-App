1210dde9536dff36d4a97ac8d5e8707d5e665b01
vvvvvv eeeeee <hiddenether@yahoo.com>
2026-08-07 04:17:12 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest/config" />
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    clearMocks: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+  }
+})
